93dce1a59f124223921feb3466815d26fbae1297
Unai Murillo Mesa <RayanOujja_UnaiMurillo@outlook.com>
2025-01-26 23:32:42 +0100

    Prueba de sockets funcionando

--- src/main/java/org/umm/httpserver/HttpServer.java
+++ src/main/java/org/umm/httpserver/HttpServer.java
@@ -3,6 +3,12 @@ package org.umm.httpserver;
 import org.umm.httpserver.config.Configuration;
 import org.umm.httpserver.config.ConfigurationManager;
 
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
+import java.net.ServerSocket;
+import java.net.Socket;
+
 public class HttpServer {
 
     public static void main(String[] args) {
@@ -14,6 +20,35 @@ public class HttpServer {
 
         System.out.println("Puerto: " + configuration.getPort());
         System.out.println("Webroot: " + configuration.getWebroot());
+
+        try {
+
+            ServerSocket serverSocket = new ServerSocket(configuration.getPort());
+            Socket socket = serverSocket.accept();
+
+            InputStream inputStream = socket.getInputStream();
+            OutputStream outputStream = socket.getOutputStream();
+
+            String html = "<html><head><title>Servidor HTTP</title></head><body><h1>Hola que tal</h1></body></html>";
+
+            final String CRLF = "\r\n";
+
+            String response = "HTTP/1.1 200 OK" + CRLF +
+                    "Tamaño del mensaje" + html.getBytes().length + CRLF +
+                    CRLF +
+                    html +
+                    CRLF + CRLF;
+
+            outputStream.write(response.getBytes());
+
+            inputStream.close();
+            outputStream.close();
+            socket.close();
+            serverSocket.close();
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
 
     }
 
