ba90195b0fd151bebb017ee938f798c77c429424
UnaiMurilloMesa <unaimurillo123@gmail.com>
2025-03-16 16:30:00 +0100

    Creación tests http y Objeto Parser

---
+++ src/main/java/org/umm/httpserver/http/HttpParser.java
@@ -0,0 +1,16 @@
+package org.umm.httpserver.http;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.io.InputStream;
+
+public class HttpParser {
+
+    private final static Logger log = LoggerFactory.getLogger(HttpParser.class);
+
+    public void parseHttpRequest(InputStream inputStream) {
+
+    }
+
+}
---
+++ src/test/java/org/umm/httpserver/http/HttpParserTest.java
@@ -0,0 +1,51 @@
+package org.umm.httpserver.http;
+
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.TestInstance;
+import org.umm.httpserver.HttpServer;
+
+import java.io.ByteArrayInputStream;
+import java.io.InputStream;
+import java.nio.charset.StandardCharsets;
+
+import static org.junit.jupiter.api.Assertions.*;
+@TestInstance(TestInstance.Lifecycle.PER_CLASS)
+class HttpParserTest {
+
+    private HttpParser httpParser;
+
+    @BeforeAll
+    public void beforeClassMethod() {
+        httpParser = new HttpParser();
+    }
+
+    @Test
+    void parseHttpRequest() {
+        httpParser.parseHttpRequest(generateValidTestCase());
+    }
+
+    private InputStream generateValidTestCase() {
+        String rawData = "/ HTTP/1.1\r\n" +
+                "Host: localhost:8080\r\n" +
+                "Connection: keep-alive\r\n" +
+                "Cache-Control: max-age=0\r\n" +
+                "sec-ch-ua: \"Chromium\";v=\"134\", \"Not:A-Brand\";v=\"24\", \"Google Chrome\";v=\"134\"\r\n" +
+                "sec-ch-ua-mobile: ?0\r\n" +
+                "sec-ch-ua-platform: \"Windows\"\r\n" +
+                "Upgrade-Insecure-Requests: 1\r\n" +
+                "User-Agent: Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/134.0.0.0 Safari/537.36\r\n" +
+                "Accept: text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.7\r\n" +
+                "Sec-Fetch-Site: none\r\n" +
+                "Sec-Fetch-Mode: navigate\r\n" +
+                "Sec-Fetch-User: ?1\r\n" +
+                "Sec-Fetch-Dest: document\r\n" +
+                "Accept-Encoding: gzip, deflate, br, zstd\r\n" +
+                "Accept-Language: en-GB,en;q=0.9,es;q=0.8,en-US;q=0.7,sv;q=0.6\r\n" +
+                "\r\n";
+        InputStream inputStream = new ByteArrayInputStream(rawData.getBytes(StandardCharsets.US_ASCII));
+
+        return inputStream;
+    }
+}
